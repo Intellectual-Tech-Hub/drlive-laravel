76069363d32b68d4e7a95f9e43eecf4b17ba8f83
Mohammed-Thamnees <thamnees611@gmail.com>
2022-01-25 16:39:35 +0530

    sidebar edited

--- resources/views/admin/layouts/head.blade.php
+++ resources/views/admin/layouts/head.blade.php
@@ -16,4 +16,5 @@
     <link href="{{ asset('assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 
     <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
+    
 </head>
